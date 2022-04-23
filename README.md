bdb9ff24a7e8c8909601e6cac4399b2d7bf7397b
Per Thomas Jahr <perja@extern.uio.no>
2022-04-23 15:38:24 +0200

    Cleanup a bit, add images and convert to KiCAD 6.

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
-# IR Beacon
+# IR Beacon for Roomba
 
 This project describes an IR beacon that can be used with certain Roomba vacuum cleaners. The beacon can be used to prevent the vacuum cleaner from entering a room or other off-limit zones. The vacuum cleaner will simply turn around when it receives the infrared light emitted from the beacon.
 
 The project is inspired by a similar project from http://gregthielen.me/blogstuff/2017/06/27/roomba-virtual-wall/ and the timings for IR LED is taken directly from that page.
+
+![PCB side 2](images/pcb_2.jpg)
 
 ## Design
 
@@ -20,13 +22,13 @@ There are two modes of operation:
 
 # Push button on/off switch
 
-The circuit uses a power latch implemented by using special instructions that skips resetting a particular variable to zero during reset (see the _power_ variable in the code).  The push button is connected to the RESET pin on and when pressed it will reset the MCU. The setup function toggles the _power_ variable and then circuit either goes to sleep or starts blinking the LED. With such a design it is important to have low sleep current.
+The circuit uses a power latch implemented by using special instructions that skips resetting a particular variable to zero during reset (see the _power_ variable in the code).  The push button is connected to the RESET pin on and when pressed it will reset the MCU. The setup function toggles the _power_ variable and then circuit either goes to sleep or starts blinking the LED. With such a design it is important to have low sleep current. The solution is described here: http://www.technoblogy.com/show?VOO.
 
 # Configurable on-duration
 
 Depending on the route the vacuum cleaner chooses one can decide how many hours the device will be on. The user can configure this by setting two DIP switches. The duration is measured by using the number of milliseconds since the device was reset. The accuracy of this timer is not very good and can be off by several minutes. But for this purpose it works well enough.
 
-# Detect low battery
+# Detect low battery (not implemented)
 
 On start up the indicator LED will blink rapidly if it is time to replace the batteries (around 2V). This is implemented by setting the internal reference voltage as ADC input and the target VCC as the ADC reference. The solution is described in this application note from Microchip: http://ww1.microchip.com/downloads/en/Appnotes/00002447A.pdf
 
@@ -40,7 +42,20 @@ On start up the indicator LED will blink rapidly if it is time to replace the ba
 
 The PCB is designed so that it fits the Hammond 1591 ABS multipurpose enclosure and can be mounted directly without any standoffs. Only three additional holes are needed. Using one of the transparent boxes makes it easy to see where to drill. Note that there is a slight angle on the sides so the box wont stand exactly at 90 degrees, but close enough.
 
+# Improvements
 
+List of improvements that can be done:
+
+* Clean up the layout and make it more compact.
+* Replace delay in code with timers.
+* Blink the LED now and then to show that it is on.
+
+
+# Images
+
+![Front](images/front.jpg)
+![Inside enclousre](images/inside.jpg)
+![PCB side 1](images/pcb_1.jpg)
 
 
 
--- src/IR_Beacon/IR_Beacon.ino
+++ src/IR_Beacon/IR_Beacon.ino
@@ -11,59 +11,56 @@
 // Pull reset pin low with push button for toggling power.
 volatile uint8_t power __attribute__ ((section (".noinit")));
 
-int IRledPin = 0;
-int ledPin = 1;
-int dipPin1 = 4;
-int dipPin2 = 3;
+const int IRledPin = 0;
+const int ledPin = 1;
+const int dipPin1 = 4;
+const int dipPin2 = 3;
+
 uint32_t start_time;
 uint8_t timer_config;
 
-uint32_t off_time[4] = {
-  UINT32_MAX,
-  60 * MINUTE_MILLIS,
-  120 * MINUTE_MILLIS,
-  180 * MINUTE_MILLIS
+const uint32_t on_duration_ms[4] = {
+    UINT32_MAX,
+    60 * MINUTE_MILLIS,
+    120 * MINUTE_MILLIS,
+    180 * MINUTE_MILLIS
 };
 
 void setup() {
-  set_sleep_mode(SLEEP_MODE_PWR_DOWN);
-  
-  pinMode(IRledPin, OUTPUT);
-  pinMode(ledPin, OUTPUT);
-  pinMode(dipPin1, INPUT_PULLUP);
-  pinMode(dipPin2, INPUT_PULLUP);
-  timer_config = (digitalRead(dipPin1) << 1) | digitalRead(dipPin2);
-  pinMode(dipPin1, INPUT);
-  pinMode(dipPin2, INPUT);
+    set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+    pinMode(IRledPin, OUTPUT);
+    pinMode(ledPin, OUTPUT);
+    pinMode(dipPin1, INPUT_PULLUP);
+    pinMode(dipPin2, INPUT_PULLUP);
+    timer_config = (digitalRead(dipPin1) << 1) | digitalRead(dipPin2);
 
-  // Turn off ADC to save power.
-  ADCSRA &= ~ bit(ADEN);
+    // Turn off ADC to save power.
+    ADCSRA &= ~ bit(ADEN);
+    power_adc_disable();
 
-  toggle_power();
-  if (power == 0) {
-    blink(2);
-    go_to_sleep();
-  } else {
-    blink(1);
-  }
+    toggle_power();
+    if (power == 0) {
+        blink(2);
+        go_to_sleep();
+    } else {
+        blink(1);
+    }
 
-  start_time = millis();
+    start_time = millis();
 }
 
-void loop()
-{
-  SendIRCode();
-  // TODO: use sleep with watchdog timer, but this timer is not accurate enough.
-  // Use SLEEP_IDLE to save some power and then use that to find when 132 ms have passed. 
-  // Configure WD to sleep for 32 ms (32, 64, 96, 128)
-  // Current AVG current is 3.3 mA when IR is blinking.
-  delay(132);
-  
-  if (millis() - start_time > off_time[timer_config]) {
-    blink(2);
-    toggle_power();
-    go_to_sleep();
-  }
+void loop() {
+    send_ir_code();
+    delay(132);
+
+    // Turn everything off after the configured duration. This will
+    // put the MCU in sleep mode and the reset button must be pushed
+    // to turn it on again.
+    if (millis() - start_time > on_duration_ms[timer_config]) {
+        blink(2);
+        toggle_power();
+        go_to_sleep();
+    }
 }
 
 void blink(int blinks) {
@@ -80,15 +77,21 @@ void toggle_power() {
 }
 
 void go_to_sleep() {
-    power_all_disable();  // power off ADC, Timer 0 and 1, serial interface
+    // Set the dip pins to INPUT to save power.
+    pinMode(dipPin1, INPUT);
+    pinMode(dipPin2, INPUT);
+
+    // Power off ADC, Timer 0 and 1, serial interface.
+    power_all_disable();
     sleep_enable();
     sleep_cpu();
+
+    // Power everything back on.
     sleep_disable();
-    power_all_enable();   // power everything back on
+    power_all_enable();
 }
 
-void IR(long microsecs) {
-  //cli();
+void ir(long microsecs) {
   while (microsecs > 0) {
     digitalWrite(IRledPin, HIGH);
     delayMicroseconds(10);
@@ -96,14 +99,11 @@ void IR(long microsecs) {
     delayMicroseconds(10);
     microsecs -= 26;
   }
-  //sei();
 }
 
-void SendIRCode() {
+void send_ir_code() {
   for (int i = 0; i < BURST_COUNT; i++) {
-    IR(BURST_ON);
+    ir(BURST_ON);
     delayMicroseconds(BURST_OFF);
   }
 }
-
-
